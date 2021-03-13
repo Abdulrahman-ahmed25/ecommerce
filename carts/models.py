a806178bdd202af7a45ae2b9f664d2dfb69de150
Abdulrahman Ahmed <abdulrahman.ahmed2544@gmail.com>
2021-03-13 10:43:54 +0200

    update cart app

--- carts/models.py
+++ carts/models.py
@@ -1,15 +1,36 @@
+from decimal import Decimal
 from django.db import models
 from django.conf import settings
 from django.db.models.fields.related import ForeignKey
 from products.models import Variation
+from django.urls import reverse
+from django.db.models.signals import pre_save
 # Create your models here.
 class CartItem(models.Model):
     cart = models.ForeignKey('Cart', on_delete=models.CASCADE)
     item = models.ForeignKey(Variation, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    line_item_total = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.item.title
+    def get_title(self):
+        return f'{self.item.product.title} - {self.item.title}'
+    
+    # def add_to_cart(self):
+    #     return 0
+    def remove(self):
+        return f'{reverse("cart")}?item={self.item.id}&delete=True'
+
+# to calculate the line_item_total
+def cart_item_pre_save_receiver(sender, instance, *args, **kwargs):
+    qty = instance.quantity
+    if Decimal(qty) >= 1 :
+        price = instance.item.get_price()
+        line_item_total = Decimal(price) * Decimal(qty)
+        instance.line_item_total = line_item_total
+        
+pre_save.connect(cart_item_pre_save_receiver, sender=CartItem)
 
 class Cart(models.Model):
     #user
--- carts/views.py
+++ carts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, Http404
 from django.views.generic.base import View
 from django.views.generic.detail import SingleObjectMixin
 from products.models import Variation
@@ -7,6 +7,8 @@ from .models import Cart, CartItem
 # Create your views here.
 
 class CartView(SingleObjectMixin, View):
+    model = Cart
+    template_name = 'carts/view.html'
     def get_object(self, *args, **kwargs):
         self.request.session.set_expiry(0) #value is 0,thats meaning the user’s session cookie will expire when the user’s Web browser is closed.
         cart_id = self.request.session.get('cart_id')
@@ -16,8 +18,7 @@ class CartView(SingleObjectMixin, View):
             cart_id = cart.id
             self.request.session['cart_id'] = cart_id
         cart = Cart.objects.get(id=cart_id)
-
-        if self.request.user.is_authenticated():
+        if self.request.user.is_authenticated:
             cart.user = self.request.user  
             cart.save()
         return cart
@@ -29,12 +30,21 @@ class CartView(SingleObjectMixin, View):
         if item_id:
             item_instance = get_object_or_404(Variation, id= item_id)
             #quantity
-            qty = request.GET.get('qty')
-            cart = Cart.objects.all().first()
+            qty = request.GET.get('qty', 1)
+            try:
+                if int(qty) < 1:
+                    delete_item = True
+            except:
+                raise Http404
+
             cart_item = CartItem.objects.get_or_create(cart= cart, item= item_instance)[0]
             if delete_item:
                 cart_item.delete()
             else:
                 cart_item.quantity = qty
                 cart_item.save()
-        return HttpResponseRedirect('/')
+        context = {
+            "object":self.get_object()
+        }
+        template = self.template_name 
+        return render(request, template, context)
